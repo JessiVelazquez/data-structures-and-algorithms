b3764f71b4c3e38294bf285369d3059ee0b6945f
Jessi <jessivelazq1@gmail.com>
2021-05-07 19:03:22 -0700

    started binary tree

---
+++ javascript/code-challenges/challenge14/list.js
@@ -0,0 +1,32 @@
+'use strict'
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+let duckDuckGoose = (list, k) => {
+  let Joe = new Node(Joe);
+  let Karen = new Node(Karen);
+  let Brian = new Node(Brian);
+  let Steph = new Node(Steph);
+  let Max = new Node(Max);
+
+  let list = new LinkedList()
+
+}
+
+let duckDuckGoose = (list, k) => {
+  let answer = '';
+  while(list.head){
+
+  }
+}
