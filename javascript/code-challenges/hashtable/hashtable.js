bb07ebe24d94aa99602be76f74dff011d7e5f112
Jessi <jessivelazq1@gmail.com>
2021-06-05 10:57:15 -0700

    all hashmap functions working

---
+++ javascript/code-challenges/hashtable/hashtable.js
@@ -0,0 +1,107 @@
+'use strict';
+
+//-------------NODE CLASS---------\\
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+//------------LINKED LIST CLASS---------\\
+class LL {
+  constructor() {
+    this.head = null;
+  }
+
+  add(val) {
+    let node = new Node(val);
+    if(!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
+  getList() {
+    if (!this.head) return 'no list';
+    let current = this.head;
+    while(current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+}
+
+//------------HASHMAP CLASS----------------\\
+class Hashmap {
+  constructor(size) {
+    this.storage = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let count = 1;
+    return key.split('').reduce((accumulator, current) => {
+      count = count + 1;
+      return accumulator + (current.charCodeAt(0) * count);
+    }, 0) * 19 % this.size;
+  }
+
+  contains(key) {
+    let hash = this.hash(key);
+    if (this.storage[hash]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  set(key, val) {
+    let hash = this.hash(key);
+
+    if(!this.storage[hash]) {
+      let ll = new LL();
+      ll.add([key, val]);
+      this.storage[hash] = ll;
+    } else {
+      let data = [key, val];
+      this.storage[hash].add(data);
+    }
+  }
+
+  get(key) {
+    let hash = this.hash(key);
+    let ll = this.storage[hash];
+    if (!this.storage[hash]) {
+      console.log('no list');
+      return 'no list';
+    } else {
+      let current = ll.head;
+      while(current) {
+        console.log(current.val);
+        current = current.next;
+      }
+    }
+  }
+}
+
+//------------EXECUTABLE--------------\\
+
+let hashmap = new Hashmap(4000);
+
+hashmap.set('jessi', 'velazquez');
+hashmap.set('alex', 'johnson');
+hashmap.set('alex', 'boberson');
+hashmap.set('alex', 'rodriguez');
+hashmap.set('molly', 'molecule');
+hashmap.set('bob', 'smith');
+hashmap.set('jessi', 'cooper');
+hashmap.set('dog', 'theBountyHunter');
+hashmap.set('god', 'dammit');
+
+
+hashmap.get('alex');
+
+
