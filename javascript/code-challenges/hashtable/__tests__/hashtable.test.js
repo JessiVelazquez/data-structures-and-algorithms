c4eb397aad90d1b74cc70c83c5f5c9f969175e63
Jessi <jessivelazq1@gmail.com>
2021-06-05 12:41:34 -0700

    completed hash table code challenge

--- javascript/code-challenges/hashtable/__tests__/hashtable.test.js
+++ javascript/code-challenges/hashtable/__tests__/hashtable.test.js
@@ -1,34 +1,56 @@
 'use strict';
 
-const hashMap = require('../hashmap.js');
+const Hashmap = require('../hashtable.js');
 
-describe('HashMap', () => {
+describe('Hashmap', () => {
 
   it('Adding a key/value to your hashtable results in the value being in the data structure', () => {
-    let array = [8,4,23,42,16,15];
-    let answer = quickSort(array, 0, array.length - 1);
-    console.log(answer);
-    expect(answer).toEqual([4,8,15,16,23,42]);
+    let hashmap = new Hashmap(4000);
+    hashmap.set('st.', 'vincent');
+    let test = hashmap.contains('st.');
+    expect(test).toEqual(true);
   });
 
   it('Retrieving based on a key returns the value stored', () => {
-
+    let hashmap = new Hashmap(4000);
+    hashmap.set('dua', 'lipa');
+    let test = hashmap.get('dua');
+    expect(test).toEqual([['dua', 'lipa']]);
   });
 
   it('Successfully returns null for a key that does not exist in the hashtable', () => {
-
+    let hashmap = new Hashmap(4000);
+    hashmap.set('sharon', 'van etten');
+    let test = hashmap.get('prince');
+    expect(test).toEqual('no list');
   });
 
   it('Successfully handle a collision within the hashtable', () => {
-
+    let hashmap = new Hashmap(4000);
+    hashmap.set('big', 'theif');
+    hashmap.set('big', 'boi');
+    hashmap.set('big', 'star');
+    let test = hashmap.get('big');
+    expect(test).toEqual([ [ 'big', 'star' ], [ 'big', 'boi' ], [ 'big', 'theif' ] ]);
   });
 
   it('Successfully retrieve a value from a bucket within the hashtable that has a collision', () => {
-
+    let hashmap = new Hashmap(4000);
+    hashmap.set('big', 'theif');
+    hashmap.set('big', 'boi');
+    hashmap.set('big', 'star');
+    let test = hashmap.get('big');
+    expect(test[1]).toEqual([ 'big', 'boi' ]);
   });
 
   it('Successfully hash a key to an in-range value', () => {
-
+    let hashmap = new Hashmap(4000);
+    let hash1 = hashmap.hash('king gizzard and the lizard wizard');
+    let hash2 = hashmap.hash('sigur ros()');
+    let hash3 = hashmap.hash('X');
+    expect(hash1 < 4000).toBe(true);
+    expect(hash2 < 4000).toBe(true);
+    expect(hash3 < 4000).toBe(true);
   });
 
 });
